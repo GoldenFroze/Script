f95c4d53ea00de1e1cb46c3e63cd0a96e8a368f6
GoldenFroze <vertuzca@gmail.com>
2021-08-07 19:12:47 +0800

    Add files via upload

---
+++ fluid/ginkgo-clone_sources.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+git clone https://github.com/trinket-devs/device_xiaomi_ginkgo.git -b 11.0 device/xiaomi/ginkgo &&
+git clone https://github.com/trinket-devs/vendor_xiaomi_ginkgo.git -b 11.0 vendor/xiaomi/ginkgo &&
+git clone https://github.com/trinket-devs/vendor_miuicamera.git -b 11.0 vendor/miuicamera &&
+git clone https://github.com/trinket-devs/kernel_xiaomi_ginkgo.git -b 11.0 kernel/xiaomi/ginkgo
