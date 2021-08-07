ad8d6061cd671225c3755c4b23f4b14b050836cc
GoldenFroze <vertuzca@gmail.com>
2021-08-07 19:18:37 +0800

    Delete ginkgo-clone_sources.sh

--- ginkgo-clone_sources.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_ginkgo.git -b arrow-11.0 device/xiaomi/ginkgo &&
-git clone https://github.com/ArrowOS-Devices/android_vendor_xiaomi_ginkgo.git -b arrow-11.0 vendor/xiaomi/ginkgo &&
-git clone https://github.com/ArrowOS-Devices/android_vendor_miuicamera.git -b arrow-11.0-a3 vendor/miuicamera &&
-git clone https://github.com/ArrowOS-Devices/android_kernel_xiaomi_ginkgo.git -b arrow-11.0 kernel/xiaomi/ginkgo
